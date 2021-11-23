0801c8c45c10698024254db27a9946b260ac407a
Seungho Han <vkwnwlzla@gmail.com>
2021-11-23 18:57:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -29,4 +29,4 @@ for img_id in range(num_img):
 	text = "Coordinates: x=%2fm y=%2fm z=%2fm"%(x,y,z)
 	cv2.putText(traj, text, (20,40), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1, 8)
 
-cv2.imwrite('map.png', traj)
+cv2.imwrite('result.png', traj)
